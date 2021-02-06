ba923fe13679fef9b2411af7b4a7df8e515bd95a
hgnis <gangwar.jar@gmail.com>
2021-02-06 13:19:57 +0530

    Initial commit

---
+++ mLoader/src/main/java/com/theaverageguy/mloader/mPulse.java
@@ -0,0 +1,51 @@
+package com.theaverageguy.mloader;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
+import android.widget.TextView;
+
+public class mPulse {
+    Context context;
+    private Dialog dialog;
+
+    public mPulse(Context context) {
+        this.context = context;
+        dialog = new Dialog(context, R.style.DialogFragmentTheme);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.pulse);
+        dialog.setCancelable(false);
+    }
+
+    public void showLoader() {
+        if (dialog.isShowing()) {
+            dialog.dismiss();
+        }
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    public void showLoader(String text) {
+        TextView textView = dialog.findViewById(R.id.title);
+        textView.setText(text);
+        if (!dialog.isShowing()) {
+            dialog.show();
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+
+    public void hideLoader() {
+        if (dialog.isShowing()) {
+            dialog.dismiss();
+        }
+
+    }
+
+    public boolean isShowing() {
+        return dialog.isShowing();
+    }
+}
+
+
---
+++ mLoader/src/main/java/com/theaverageguy/mloader/mRipple.java
@@ -0,0 +1,50 @@
+package com.theaverageguy.mloader;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Window;
+import android.widget.TextView;
+
+public class mRipple {
+
+    Context context;
+    private Dialog dialog;
+
+    public mRipple(Context context) {
+        this.context = context;
+        dialog = new Dialog(context, R.style.DialogFragmentTheme);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.ripple);
+        dialog.setCancelable(false);
+    }
+
+    public void showLoader() {
+        if (dialog.isShowing()) {
+            dialog.dismiss();
+        }
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    public void showLoader(String text) {
+        TextView textView = dialog.findViewById(R.id.title);
+        textView.setText(text);
+        if (!dialog.isShowing()) {
+            dialog.show();
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+
+    public void hideLoader() {
+        if (dialog.isShowing()) {
+            dialog.dismiss();
+        }
+
+    }
+
+    public boolean isShowing() {
+        return dialog.isShowing();
+    }
+}
